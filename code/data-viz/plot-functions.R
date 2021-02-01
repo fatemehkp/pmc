4a06531cb2d610a2564294e6db515ade66f27d8b
Fatemeh Kazemi <fkazem01@tufts.edu>
2021-01-31 23:16:00 -0500

    CMS, config files, output cleaning for factor em analysis - PMC data summary - Plot functions - plot making

---
+++ code/data-viz/plot-functions.R
@@ -0,0 +1,72 @@
+### Author: Fatemeh Kazemi - 01-30-2021
+
+# Functions for plotting results of exposure ~ outcome analysis
+# Hazard Ratio (HR) and 95% CI (HR.L, HR.U) (HR.Lower and HR.Upper)
+
+library(tidyverse)
+library(ggpubr)
+
+### Function *** Plot.Single ***
+# Function Arguments: 
+# dt : dataset that contains the HRs for each exposure and outcome
+
+### Single Group Plot Function
+Plot.Single <- function(dt, ggtheme){
+  p <- ggplot(data, aes(x = HR, xmin = HR.L, xmax = HR.U, y = Index)) +
+    geom_vline(xintercept = 1, linetype = 2, color = "black") +
+    labs(x = "", y = "") +
+    geom_segment(aes(x = HR.L, xend = HR.U, y = Index, yend = Index),
+                 color = plot.cols, size = 0.25, show.legend = F) + 
+    geom_point(aes(x = HR, y = Index, shape = sig), 
+               color = plot.cols, size = 1.5, show.legend = F) +
+    scale_shape_identity() +
+    facet_wrap( ~ Cause, ncol = 5, scales = "fixed") +
+    ggtheme
+  return(p)
+}
+
+
+### Function *** Plot.Multi ***
+# Function Arguments: 
+# dt : dataset that contains the HRs for each exposure, outcome and subgroups
+
+### Multiple Group Plot Function
+Plot.Multi <- function(dt, ggtheme) {
+  p1 <- ggplot(data = filter(data, Cause %in% cuz.top.row),
+               aes(x = HR, xmin = HR.L, xmax = HR.U, y = Tag)) +
+    geom_vline(xintercept = 1, linetype = 2, color = "black") +
+    labs(x = "", y = "") +
+    geom_segment(aes(x = HR.L, xend = HR.U, y = Tag, yend = Tag, color = Tag),
+                 size = 0.25, show.legend = T) + 
+    geom_point(aes(x = HR, y = Tag, color = Tag, shape = sig), 
+               size = 1.5, show.legend = T) + 
+    scale_shape_identity() +
+    scale_color_manual(
+      labels = plot.labs, 
+      values = plot.cols) +
+    
+    facet_grid(Index ~ Cause,  scales = "free_x", switch = 'y',
+               shrink = T, space = 'fixed') +
+    ggtheme +
+    guides(colour = guide_legend(nrow = 1, byrow = T, reverse = T, direction = "vertical")) 
+  
+  p2 <- ggplot(data = filter(data, Cause %in% cuz.bot.row),
+               aes(x = HR, xmin = HR.L, xmax = HR.U, y = Tag)) +
+    geom_vline(xintercept = 1, linetype = 2, color = "black") +
+    labs(x = "", y = "") +
+    geom_segment(aes(x = HR.L, xend = HR.U, y = Tag, yend = Tag, color = Tag),
+                 size = 0.25, show.legend = T) + 
+    geom_point(aes(x = HR, y = Tag, color = Tag, shape = sig), 
+               size = 1.5, show.legend = T) + 
+    scale_shape_identity() +
+    scale_color_manual(
+      labels = plot.labs, 
+      values = plot.cols) +
+    facet_grid(Index ~ Cause,  scales = "free_x", switch = 'y',
+               shrink = T, space = 'fixed') +
+    ggtheme +
+    guides(colour = guide_legend(nrow = 1, byrow = T, reverse = T, direction = "vertical"))
+  
+  p <- ggarrange(p1, p2, common.legend =T, nrow = 2, legend = "bottom")
+  return(p)
+}
